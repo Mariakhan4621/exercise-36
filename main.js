762820f939e74d50086240aa311806c2258b31b8
Mariakhan4621 <160171512+Mariakhan4621@users.noreply.github.com>
2024-03-15 18:18:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function makeshirt(size, text) {
+    console.log("/n you order a ".concat(size, " shirt that says ").concat(text));
+}
+makeshirt('large', '- i love typesript-');
+makeshirt('medium', '-i need a big shirt-');
---
+++ main.ts
@@ -0,0 +1,6 @@
+function makeshirt(size: string, text: string): void {
+    console.log(`/n you order a ${size} shirt that says ${text}`)
+}
+
+makeshirt('large' , '- i love typesript-')
+makeshirt('medium', '-i need a big shirt-')
